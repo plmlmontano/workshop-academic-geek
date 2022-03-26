6a4639bf13832218655902d42022e52cff0e26ea
Juan Hoyos <juanhoyos1347@gmail.com>
2022-03-25 20:11:32 -0500

    modificacion del recorrido de data

--- rick_morty_project/scripts/app.js
+++ rick_morty_project/scripts/app.js
@@ -1,25 +1,30 @@
-import { characters } from "../data/data.js";
-import { Carta} from "../modules/carta.js";
+import {
+    characters
+} from "../data/data.js";
+import {
+    Carta
+} from "../modules/carta.js";
 
 let containerCarta = document.querySelector('.carta-box');
 
 
-document.addEventListener('click', (e)=>{
-    if(e.target.classList.contains('cart')){
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('cart')) {
         cambiarCara(e);
     }
 })
 
 
-function cambiarCara(evento){
+function cambiarCara(evento) {
     console.log("Se cambia la cara");
     console.log(evento.target.id);
 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    characters.forEach((elemento,index)=>{
-        Carta(containerCarta,elemento.image,elemento.id);
-    })
+    let arregloNuevo = [...characters, ...characters]
+    for (let i = 0; i < arregloNuevo.length - 30; i++) {
+        Carta(containerCarta,arregloNuevo[i].image,arregloNuevo[i].id)
+    }
     console.log(containerCarta);
 });
