03faedcc896cb8bea1ef996343ce6ad3c3eb072f
Juan Hoyos <juanhoyos1347@gmail.com>
2022-03-25 20:13:17 -0500

    settings for vscode

--- rick_morty_project/modules/carta.js
+++ rick_morty_project/modules/carta.js
@@ -1,10 +1,10 @@
 export const Carta = (contenedorCarta,cartaAtras,id) => {
     contenedorCarta.innerHTML += `
-    <div class="carta">
-      <div class="cara">
-          <img src="img/image1.png" width="200" height="250px" class="cart" id="${id}">
+    <div class="card">
+      <div class="front">
+          <img src="img/image1.png" width="200" height="250px" class ="front-card"id="${id}">
       </div>
-      <div class="cara detras">
+      <div class="back">
         <img src="${cartaAtras}" width="200" height="250px">
       </div>
     </div>
